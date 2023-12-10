bf21b0b18eb2862cc5eb0ecbb342973c63a9a377
Matthew Horwood <matt@horwood.biz>
2023-12-10 09:14:54 +0000

    Alpine 3.19 & PHP 8.2
    feat: update OS to 3.19
    feat: update PHP to 8.2

--- docker/config/start.sh
+++ docker/config/start.sh
@@ -5,9 +5,9 @@ ln -s /dev/stdout /var/log/nginx/access.log
 
 if [ "$TZ" != "" ]
 then
-  sed -i "s!Europe/London!$TZ!" /etc/php81/php.ini
+  sed -i "s!Europe/London!$TZ!" /etc/php82/php.ini
 fi
 
-php-fpm81 -R
+php-fpm82 -R
 
 exec "$@"
