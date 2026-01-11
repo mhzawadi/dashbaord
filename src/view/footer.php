79e0c2041f365bd87d940bee57b44ed5ef6bbd0c
Matthew Horwood <matt@horwood.biz>
2026-01-11 13:37:29 +0000

    feat: pre-load assets chor: tidy code

--- src/view/footer.php
+++ src/view/footer.php
@@ -1,7 +1,7 @@
       </div>
     </div>
-    <script src="https:////code.iconify.design/1/1.0.6/iconify.min.js"></script>
-    <script src="/js/bundle.js"></script>
+    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+    <script src="/js/bundle.min.js"></script>
   </body>
 </html>
 <script>
