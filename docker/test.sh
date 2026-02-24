5d68c7e5dcfc602ed8d7fbf5205cf7a359500f62
Matthew Horwood <matt@horwood.biz>
2026-02-24 21:02:32 +0000

    chor: update composer

--- docker/test.sh
+++ docker/test.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
 
-docker compose -f docker/docker-compose.yml down;
-docker image rm mhzawadi/dashbaord:dev;
-docker build -t mhzawadi/dashbaord:dev -f ./docker/Dockerfile-dev . && \
-docker compose -f docker/docker-compose.yml up
+if [ $# -lt 1 ]
+then
+  docker scout quickview fs://.
+  docker scout cves fs://.
+  docker run --rm -t -v "${PWD}":/workdir overtrue/phplint:latest ./ --exclude=vendor --no-configuration --no-cache && \
+  docker compose -f docker-compose-dev.yml up foxess-mqtt
+elif [ "$1" == "composer" ]
+then
+  docker image rm mhzawadi/dashbaord:dev;
+  docker build -t mhzawadi/dashbaord:dev -f ./docker/Dockerfile-dev . && \
+  docker run --rm -it -v '/Users/matt/git/dashbaord:/var/www/html' mhzawadi/dashbaord:dev /usr/local/bin/composer update
+  docker compose -f docker-compose-dev.yml up -d
+elif [ "$1" == "up" ]
+then
+  docker image rm mhzawadi/dashbaord:dev && \
+  docker build -t mhzawadi/dashbaord:dev -f ./docker/Dockerfile-dev . && \
+  docker run --rm -t -v "${PWD}":/workdir overtrue/phplint:latest ./ --exclude=vendor --no-configuration --no-cache
+else
+  docker compose -f docker-compose-dev.yml down
+fi
