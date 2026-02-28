eab9db5c1b511ce58890799f7fba8b536c2d6f82
Matthew Horwood <matt@horwood.biz>
2026-02-28 17:48:24 +0000

    bug: sort the link colour bug: fix test script

--- docker/test.sh
+++ docker/test.sh
@@ -14,9 +14,10 @@ then
   docker compose -f docker-compose-dev.yml up -d
 elif [ "$1" == "up" ]
 then
-  docker image rm mhzawadi/dashbaord:dev && \
+  docker compose -f docker/docker-compose.yml down;
+  docker image rm mhzawadi/dashbaord:dev;
   docker build -t mhzawadi/dashbaord:dev -f ./docker/Dockerfile-dev . && \
-  docker run --rm -t -v "${PWD}":/workdir overtrue/phplint:latest ./ --exclude=vendor --no-configuration --no-cache
+  docker compose -f docker/docker-compose.yml up
 else
   docker compose -f docker-compose-dev.yml down
 fi
